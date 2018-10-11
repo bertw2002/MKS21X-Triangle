c11b2b5edaea00317241553c92db2b185ca6afae
Sublime <other@Alberts-MBP.fios-router.home>
2018-10-10 22:24:15 -0400

    added code

--- Triangle.java
+++ Triangle.java
@@ -1,1 +1,12 @@
+public class Triangle{
+  private Point v1, v2, v3;
 
+  public Triangle(Point, Point, Point){
+    v1 = Point;
+    v2 = Point;
+    v3 = Point;
+  }
+  public Triangle(double x1, double x2, double x3, double y1, double y2, double y3){
+
+  }
+}
